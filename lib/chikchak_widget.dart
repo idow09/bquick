63463e204c9cd3c0d4243f3605cf9b2ceb49aeda
Ido Weiss <idow09@gmail.com>
2018-12-14 14:20:06 +0200

    Added a text with a place holder to show current number

--- lib/chikchak_widget.dart
+++ lib/chikchak_widget.dart
@@ -69,6 +69,18 @@ class ChikChakGameState extends State<ChikChakGame> {
                 }).toList(),
               ),
             ),
+            Row(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    "Current number: 7",
+                    textAlign: TextAlign.start,
+                    style: TextStyle(fontSize: 25),
+                  ),
+                ),
+              ],
+            )
           ],
         );
       },
