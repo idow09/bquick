66880bdeb1403a414c5d6cb317776824830ac257
Ido Weiss <idow09@gmail.com>
2018-12-15 19:48:15 +0200

    Swapped the StreamBuilder of the GridView to be positioned near it

--- lib/chikchak_widget.dart
+++ lib/chikchak_widget.dart
@@ -17,70 +17,68 @@ class ChikChakGameState extends State<ChikChakGame> {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<List<ChikChakTile>>(
-      stream: _bloc.gameState,
-      builder:
-          (BuildContext context, AsyncSnapshot<List<ChikChakTile>> snapshot) {
-        return Column(
+    return Column(
+      children: <Widget>[
+        Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.width,
+            child: StreamBuilder(
+                stream: _bloc.gameState,
+                builder: (BuildContext context,
+                    AsyncSnapshot<List<ChikChakTile>> snapshot) {
+                  return GridView.count(
+                    physics: NeverScrollableScrollPhysics(),
+                    crossAxisCount: 5,
+                    children: snapshot.data.map((tile) {
+                      if (tile.visible) {
+                        return GestureDetector(
+                          onTap: () {
+                            _bloc.clicks.add(tile.num);
+                          },
+                          child: Tile(Text('${tile.num}')),
+                        );
+                      } else {
+                        return Container();
+                      }
+                    }).toList(),
+                  );
+                })),
+        Spacer(),
+        Row(
           children: <Widget>[
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.width,
-              child: GridView.count(
-                physics: NeverScrollableScrollPhysics(),
-                crossAxisCount: 5,
-                children: snapshot.data.map((tile) {
-                  if (tile.visible) {
-                    return GestureDetector(
-                      onTap: () {
-                        _bloc.clicks.add(tile.num);
-                      },
-                      child: Tile(Text('${tile.num}')),
-                    );
-                  } else {
-                    return Container();
-                  }
-                }).toList(),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Tile(
+                Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: StreamBuilder(
+                        stream: _bloc.curNum,
+                        builder: (BuildContext context,
+                                AsyncSnapshot<int> snapshot) =>
+                            Text(
+                              "Current: ${snapshot.data}",
+                              textAlign: TextAlign.start,
+                              style: TextStyle(fontSize: 25),
+                            ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        "High Score: 100000",
+                        textAlign: TextAlign.start,
+                        style: TextStyle(fontSize: 25),
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
-            Spacer(),
-            Row(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8.0),
-                  child: Tile(
-                    Column(
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: StreamBuilder(
-                            stream: _bloc.curNum,
-                            builder: (BuildContext context,
-                                    AsyncSnapshot<int> snapshot) =>
-                                Text(
-                                  "Current: ${snapshot.data}",
-                                  textAlign: TextAlign.start,
-                                  style: TextStyle(fontSize: 25),
-                                ),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            "High Score: 100000",
-                            textAlign: TextAlign.start,
-                            style: TextStyle(fontSize: 25),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            )
           ],
-        );
-      },
+        )
+      ],
     );
   }
 }
