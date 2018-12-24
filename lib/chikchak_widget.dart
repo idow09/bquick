1c72a05c4165cc448a133cd38fdf9b937c61f4fb
Ido Weiss <idow09@gmail.com>
2018-12-24 22:38:42 +0200

    Use stack to solve hidden shadow bug

--- lib/chikchak_widget.dart
+++ lib/chikchak_widget.dart
@@ -17,66 +17,61 @@ class ChikChakGameState extends State<ChikChakGame> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Stack(
       children: <Widget>[
-        Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.width,
-            child: StreamBuilder(
-                stream: _bloc.gameState,
-                builder: (BuildContext context,
-                    AsyncSnapshot<List<ChikChakTile>> snapshot) {
-                  return GridView.count(
-                    physics: NeverScrollableScrollPhysics(),
-                    crossAxisCount: 5,
-                    children: snapshot.data.map((tile) {
-                      if (tile.visible) {
-                        return GestureDetector(
-                          onTap: () {
-                            _bloc.clicks.add(tile.num);
-                          },
-                          child: Tile(Text('${tile.num}')),
-                        );
-                      } else {
-                        return Container();
-                      }
-                    }).toList(),
-                  );
-                })),
-        Spacer(),
-        Row(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8.0),
-              child: Tile(
-                Column(
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: StreamBuilder(
-                        stream: _bloc.curNum,
-                        builder: (BuildContext context,
-                                AsyncSnapshot<int> snapshot) =>
-                            Text(
-                              "Current: ${snapshot.data}",
-                              textAlign: TextAlign.start,
-                              style: TextStyle(fontSize: 25),
-                            ),
-                      ),
+        StreamBuilder(
+            stream: _bloc.gameState,
+            builder: (BuildContext context,
+                AsyncSnapshot<List<ChikChakTile>> snapshot) {
+              return GridView.count(
+                physics: NeverScrollableScrollPhysics(),
+                crossAxisCount: 5,
+                children: snapshot.data.map((tile) {
+                  if (tile.visible) {
+                    return GestureDetector(
+                      onTap: () {
+                        _bloc.clicks.add(tile.num);
+                      },
+                      child: Tile(Text('${tile.num}')),
+                    );
+                  } else {
+                    return Container();
+                  }
+                }).toList(),
+              );
+            }),
+        Positioned(
+          bottom: 0,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: Tile(
+              Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: StreamBuilder(
+                      stream: _bloc.curNum,
+                      builder:
+                          (BuildContext context, AsyncSnapshot<int> snapshot) =>
+                          Text(
+                            "Current: ${snapshot.data}",
+                            textAlign: TextAlign.start,
+                            style: TextStyle(fontSize: 25),
+                          ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        "High Score: 100000",
-                        textAlign: TextAlign.start,
-                        style: TextStyle(fontSize: 25),
-                      ),
-                    )
-                  ],
-                ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "High Score: 100000",
+                      textAlign: TextAlign.start,
+                      style: TextStyle(fontSize: 25),
+                    ),
+                  )
+                ],
               ),
             ),
-          ],
+          ),
         )
       ],
     );
