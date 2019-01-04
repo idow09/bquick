aa7f11d7437c9e445a3ef58abb422d6c98f96672
Ido Weiss <idow09@gmail.com>
2019-01-04 11:53:40 +0200

    Added test for random initial state

--- test/chikchak_bloc_test.dart
+++ test/chikchak_bloc_test.dart
@@ -1,14 +1,27 @@
-//import 'package:mockito/mockito.dart';
 import 'package:chik_chak/chikchak_bloc.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('Tiles ', () {
+    ChikChakBloc _bloc;
+    setUp(() {
+      _bloc = ChikChakBloc();
+    });
+
     test('are all visible', () async {
-      final _bloc = ChikChakBloc();
-      final initialState = await _bloc.gameState.first;
-      expect(
-          initialState.map((tile) => tile.visible), everyElement(equals(true)));
+      final _initialState = await _bloc.gameState.first;
+      var _initialVisibilityState = _initialState.map((tile) => tile.visible);
+
+      expect(_initialVisibilityState, everyElement(equals(true)));
+    });
+
+    test('are randomly ordered', () async {
+      final _initialState = await _bloc.gameState.first;
+      final _orderedNumList =
+          List.generate(ChikChakBloc.TILES_COUNT, (i) => i + 1);
+      var _initialNumList = _initialState.map((tile) => tile.num);
+
+      expect(_initialNumList, isNot(orderedEquals(_orderedNumList)));
     });
   });
 }
