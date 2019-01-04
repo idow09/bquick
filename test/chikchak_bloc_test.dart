a182dc54a604be8d5b080d89c63b246d9ab08fc0
Ido Weiss <idow09@gmail.com>
2019-01-04 12:25:20 +0200

    Added tests for the first click. Reordered tests.

--- test/chikchak_bloc_test.dart
+++ test/chikchak_bloc_test.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:chik_chak/chikchak_bloc.dart';
 import 'package:test/test.dart';
 
@@ -7,21 +9,47 @@ void main() {
     _bloc = ChikChakBloc();
   });
 
-  group('Tiles in initial state ', () {
-    test('are all visible', () async {
-      final _initialState = await _bloc.gameState.first;
-      var _initialVisibilityState = _initialState.map((tile) => tile.visible);
+  group('In initial state ', () {
+    group('tiles ', () {
+      UnmodifiableListView<ChikChakTile> _initialState;
+      setUp(() async => _initialState = await _bloc.gameState.first);
 
-      expect(_initialVisibilityState, everyElement(equals(true)));
+      test('are all visible', () async {
+        var _initialVisibilityState = _initialState.map((tile) => tile.visible);
+
+        expect(_initialVisibilityState, everyElement(equals(true)));
+      });
+
+      test('are randomly ordered', () async {
+        final _orderedNumList =
+            List.generate(ChikChakBloc.TILES_COUNT, (i) => i + 1);
+        var _initialNumList = _initialState.map((tile) => tile.num);
+
+        expect(_initialNumList, isNot(orderedEquals(_orderedNumList)));
+      });
     });
 
-    test('are randomly ordered', () async {
-      final _initialState = await _bloc.gameState.first;
-      final _orderedNumList =
-          List.generate(ChikChakBloc.TILES_COUNT, (i) => i + 1);
-      var _initialNumList = _initialState.map((tile) => tile.num);
+    test('current number is 1', () async {
+      final _firstCurNum = await _bloc.curNum.first;
 
-      expect(_initialNumList, isNot(orderedEquals(_orderedNumList)));
+      expect(_firstCurNum, equals(1));
+    });
+  });
+
+  group("After 1 is clicked ", () {
+    setUp(() {
+      _bloc.clicks.add(1);
+    });
+
+    test("it's no longer visible", () async {
+      var _state = await _bloc.gameState.first;
+      expect(_state.where((tile) => tile.num == 1).map((tile) => tile.visible),
+          equals([false]));
+    });
+
+    test("current number is incremented", () async {
+      var _curNum = await _bloc.curNum.first;
+      expect(_curNum, equals(2));
     });
   });
 }
