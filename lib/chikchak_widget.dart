7735db78adbddada3320f27c0db6ac0b13e59fb1
Ido Weiss <idow09@gmail.com>
2019-01-13 21:49:37 +0200

    Started refactor of view widgets

--- lib/chikchak_widget.dart
+++ lib/chikchak_widget.dart
@@ -8,124 +8,130 @@ class ChikChakGame extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
+    return Column(
       children: <Widget>[
-        Expanded(child: ChikChakGrid(_bloc)),
-        Positioned(
-          bottom: 0,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: Tile(
-              child: Row(
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          child: Row(
+            children: <Widget>[
+              Row(
                 children: <Widget>[
-                  Column(
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: StreamBuilder(
-                          stream: _bloc.bestTime,
-                          builder: (BuildContext context,
-                              AsyncSnapshot<String> snapshot) {
-                            return Row(
-                              children: <Widget>[
-                                Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 8.0),
-                                  child: Icon(Icons.arrow_upward),
-                                ),
-                                Text(
-                                  "${snapshot.data}",
-                                  textAlign: TextAlign.start,
-                                  style: TextStyle(fontSize: 25),
-                                ),
-                              ],
-                            );
-                          },
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: StreamBuilder(
-                          stream: _bloc.curStopwatch,
-                          builder: (BuildContext context,
-                              AsyncSnapshot<String> snapshot) {
-                            return Row(
-                              children: <Widget>[
-                                Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 8.0),
-                                  child: Icon(Icons.timer),
-                                ),
-                                Text(
-                                  "${snapshot.data}",
-                                  textAlign: TextAlign.start,
-                                  style: TextStyle(fontSize: 25),
-                                ),
-                              ],
-                            );
-                          },
-                        ),
-                      )
-                    ],
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: StreamBuilder(
+                      stream: _bloc.bestTime,
+                      builder: (BuildContext context,
+                          AsyncSnapshot<String> snapshot) {
+                        return Row(
+                          children: <Widget>[
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 8.0),
+                              child: Icon(Icons.arrow_upward),
+                            ),
+                            Text(
+                              "${snapshot.data}",
+                              textAlign: TextAlign.start,
+                              style: TextStyle(fontSize: 25),
+                            ),
+                          ],
+                        );
+                      },
+                    ),
                   ),
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: StreamBuilder(
-                        stream: _bloc.gameStatus,
-                        builder: (BuildContext context,
-                            AsyncSnapshot<GameStatus> snapshot) {
-                          if (snapshot.data == GameStatus.running) {
-                            return StreamBuilder(
-                              stream: _bloc.curNum,
-                              builder: (BuildContext context,
-                                      AsyncSnapshot<int> snapshot) =>
-                                  Text(
-                                    "# ${snapshot.data}",
-                                    textAlign: TextAlign.start,
-                                    style: TextStyle(fontSize: 25),
-                                  ),
-                            );
-                          } else {
-                            return Container();
-                          }
-                        }),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-        StreamBuilder(
-          stream: _bloc.gameStatus,
-          builder: (BuildContext context, AsyncSnapshot<GameStatus> snapshot) {
-            if (snapshot.data == GameStatus.finished) {
-              return Center(
-                child: Tile(
-                    child: Column(
-                  children: <Widget>[
-                    Icon(
-                      Icons.check,
-                      size: 150,
-                    ),
-                    StreamBuilder(
                       stream: _bloc.curStopwatch,
                       builder: (BuildContext context,
-                              AsyncSnapshot<String> snapshot) =>
+                          AsyncSnapshot<String> snapshot) {
+                        return Row(
+                          children: <Widget>[
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 8.0),
+                              child: Icon(Icons.timer),
+                            ),
+                            Text(
+                              "${snapshot.data}",
+                              textAlign: TextAlign.start,
+                              style: TextStyle(fontSize: 25),
+                            ),
+                          ],
+                        );
+                      },
+                    ),
+                  )
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: StreamBuilder(
+                    stream: _bloc.gameStatus,
+                    builder: (BuildContext context,
+                        AsyncSnapshot<GameStatus> snapshot) {
+                      if (snapshot.data == GameStatus.running) {
+                        return StreamBuilder(
+                          stream: _bloc.curNum,
+                          builder: (BuildContext context,
+                                  AsyncSnapshot<int> snapshot) =>
+                              Text(
+                                "# ${snapshot.data}",
+                                textAlign: TextAlign.start,
+                                style: TextStyle(fontSize: 25),
+                              ),
+                        );
+                      } else {
+                        return Container();
+                      }
+                    }),
+              ),
+            ],
+          ),
+        ),
+        Expanded(child: ChikChakGrid(_bloc)),
+        StatusTile(_bloc)
+      ],
+    );
+  }
+}
+
+class StatusTile extends StatelessWidget {
+  final _bloc;
+
+  const StatusTile(this._bloc, {Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: _bloc.gameStatus,
+      builder: (BuildContext context, AsyncSnapshot<GameStatus> snapshot) {
+        if (snapshot.data == GameStatus.finished) {
+          return Center(
+            child: Tile(
+                child: Column(
+              children: <Widget>[
+                Icon(
+                  Icons.check,
+                  size: 150,
+                ),
+                StreamBuilder(
+                  stream: _bloc.curStopwatch,
+                  builder:
+                      (BuildContext context, AsyncSnapshot<String> snapshot) =>
                           Text(
                             "${snapshot.data}",
                             textAlign: TextAlign.center,
                             style: TextStyle(fontSize: 25),
                           ),
-                    ),
-                  ],
-                )),
-              );
-            } else {
-              return Container();
-            }
-          },
-        )
-      ],
+                ),
+              ],
+            )),
+          );
+        } else {
+          return Container();
+        }
+      },
     );
   }
 }
