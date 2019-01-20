3bc88a224bb35c496f875f0462b6b381929cd19f
Ido Weiss <idow09@gmail.com>
2019-01-20 23:56:01 +0200

    Fixed functionality of stopwatch test

--- test/chikchak_bloc_test.dart
+++ test/chikchak_bloc_test.dart
@@ -12,6 +12,7 @@ class MockScoreRepository extends Mock implements ScoreRepository {}
 
 void main() {
   ChikChakBloc _bloc;
+  var _timerCallback;
 
   final ScoreRepository mockRepo = MockScoreRepository();
 
@@ -21,7 +22,10 @@ void main() {
     when(_fastStopwatch.elapsed)
         .thenAnswer((_) => Duration(seconds: counter++));
 
-    var _fastRunner = (_, c) => Timer.periodic(Duration(milliseconds: 5), c);
+    var _fastRunner = (_, c) {
+      _timerCallback = c;
+      return null;
+    };
     when(mockRepo.fetchHighScore()).thenAnswer((_) => Future.value(
         Duration(minutes: 2, seconds: 51, milliseconds: 17).inMilliseconds));
 
@@ -125,8 +129,10 @@ void main() {
   group("After 1, 3 is clicked ", () {
     setUp(() async {
       _bloc.clicks.add(1);
+      await drainStream(_bloc.gameState, 1);
+      await _timerCallback(null);
       _bloc.clicks.add(3);
-      await drainStream(_bloc.gameState, 2);
+      await drainStream(_bloc.gameState, 1);
     });
 
     test("only 1 is invisible", () async {
@@ -142,10 +148,12 @@ void main() {
     });
 
     test("stopwatch is running", () async {
-      var nonZero = isNot("00:00.000");
-      // test that stopwatch is constantly emitting time strings
-      await drainStream(_bloc.curStopwatch, 5); // ignore first zeros
-      expect(_bloc.curStopwatch, emitsInOrder([nonZero, nonZero, nonZero]));
+      var count = 0;
+      _bloc.curStopwatch.listen(expectAsync1((curStopwatch) async {
+        expect(curStopwatch, contains(count.toString()));
+        count++;
+        if (count < 10) await _timerCallback(null);
+      }, count: 10));
     });
   });
 
